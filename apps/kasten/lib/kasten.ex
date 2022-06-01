e857e58672f0dbfa800770b7ac77bf36d79c9bc8
John Ladan <john@ladan.ca>
2022-06-01 18:06:59 -0400

    Add a module for reducing the file name

--- apps/kasten/lib/kasten.ex
+++ apps/kasten/lib/kasten.ex
@@ -4,18 +4,65 @@ defmodule Kasten do
   """
 
   def scan_dir(path, opts \\ []) do
-    _scan_dir([path], opts)
+    _scan_dir([path], _scan_opts(opts))
     |> List.flatten()
   end
 
-  defp _scan_dir(paths, opts \\ [])
   defp _scan_dir([], _opts), do: []
   defp _scan_dir([path | rest], opts) do
-    case File.stat(path) do
-      {:ok, %File.Stat{type: :directory}} -> [_scan_dir(File.ls!(path) |> Enum.map(&(path <> "/" <> &1)), opts) | _scan_dir(rest, opts)]
-      {:ok, %File.Stat{type: :regular}} -> [path | _scan_dir(rest, opts) ] 
-      _ -> IO.inspect(path)
-        _scan_dir(rest, opts) 
+    if Keyword.fetch!(opts, :show_hidden) or not hidden?(path) do
+      case File.stat(path) do
+        {:ok, %File.Stat{type: :directory}} -> 
+          [_scan_dir(File.ls!(path) |> Enum.map(&:filename.join(path, &1)), opts) | _scan_dir(rest, opts)]
+        {:ok, %File.Stat{type: :regular}} -> [path | _scan_dir(rest, opts) ] 
+        _ -> IO.inspect(path)
+          _scan_dir(rest, opts) 
+      end
+    else
+      _scan_dir(rest, opts)
     end
+  end
+
+  defp _scan_opts(opts) do
+    [
+      show_hidden: Keyword.get(opts, :show_hidden, false),
+      follow_links: Keyword.get(opts, :follow_links, false)
+    ]
+  end
+
+
+  def hidden?(path) do
+    fname = :filename.basename(path)
+    cond do
+      fname == "." or fname == ".." -> false
+      String.starts_with?(fname, ".") -> true
+      true -> false
+    end
+  end
+
+  defmodule FileSet do
+    @moduledoc """
+    Store an inventory of files by basename, and account for duplicate names.
+
+    %Map{<basename> : [<absolute_name>]}
+    """
+
+    def put(set, fname) do
+      bname = :filename.basename(fname)
+      absname = :filename.absname(fname)
+      Map.update(set, bname, MapSet.new([absname]), &MapSet.put(&1, absname))
+    end
+
+    def member?(set, fname) do
+      with bname <- :filename.basename(fname),
+           absname <- :filename.absname(fname),
+           {:ok, s} <- Map.fetch(set, bname)
+      do
+        MapSet.member?(s, absname)
+      else
+        :error -> false
+      end
+    end
+
   end
 end

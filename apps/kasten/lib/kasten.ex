970ccddd220ad3c1e31a1694860f424ca1011887
John Ladan <john@ladan.ca>
2022-06-01 18:44:29 -0400

    add fetch function to the fileset

--- apps/kasten/lib/kasten.ex
+++ apps/kasten/lib/kasten.ex
@@ -53,12 +53,19 @@ defmodule Kasten do
       Map.update(set, bname, MapSet.new([absname]), &MapSet.put(&1, absname))
     end
 
+    def fetch(set, fname) do
+      with bname <- :filename.basename(fname), 
+           {:ok, s} <- Map.fetch(set, bname) do
+        {:ok, MapSet.to_list(s)}
+      end
+    end
+
     def shortest_name(set, fname) do
       with bname <- :filename.basename(fname),
            absname = :filename.absname(fname),
            {:ok, s} <- Map.fetch(set, bname) do
         if MapSet.member?(s, absname) do
-          if MapSet.size(s) == 1, do: bname, else: absname
+          if MapSet.size(s) == 1, do: {:ok, bname}, else: {:ok, absname}
         else
           :error
         end

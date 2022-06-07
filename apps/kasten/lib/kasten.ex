e0f142ac71ff78044ab44554710c9d106e48b574
John Ladan <john@ladan.ca>
2022-06-06 22:10:30 -0400

    start implementation of the Kasten GenServer

--- apps/kasten/lib/kasten.ex
+++ apps/kasten/lib/kasten.ex
@@ -2,5 +2,26 @@ defmodule Kasten do
   @moduledoc """
   Documentation for `Kasten`.
   """
+  use GenServer
+
+  @impl true
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  # GenServer callbacks {{{
+  @impl true
+  def handle_call({:lookup, name}, _from, fileset) do
+    {:reply, Kasten.FileSet.fetch(fileset, name), fileset}
+  end
+
+  @impl true
+  def handle_call({:scan_dir}, _from, _fileset) do
+    newset = Kasten.Scanner.scan_dir() |> Enum.reduce(%{}, &Kasten.FileSet.put(&2, &1))
+    {:reply, :ok, newset}
+  end
+  
+  # }}}
+
 
 end

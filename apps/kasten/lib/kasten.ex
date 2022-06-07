16c1ba37ad7dec59f4950eac6e121d491cee6aa6
John Ladan <john@ladan.ca>
2022-06-06 22:19:54 -0400

    add some client functions to the Kasten Server

--- apps/kasten/lib/kasten.ex
+++ apps/kasten/lib/kasten.ex
@@ -4,20 +4,46 @@ defmodule Kasten do
   """
   use GenServer
 
+  # Client API {{{
+  @doc """
+  Starts the registry.
+  """
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  @doc """
+  Lookup a file in the registry.
+  """
+  def lookup(server, fname) do
+    GenServer.call(server, {:lookup, fname})
+  end
+  
+  @doc """
+  Scans a directory for files (notes). This replaces the current set of files.
+  """
+  def scan_dir(server, directory \\ ".") do
+    GenServer.call(server, {:scan_dir, directory})
+  end
+  
+
+  # }}}
+
+  # GenServer callbacks {{{
+
   @impl true
   def init(:ok) do
     {:ok, %{}}
   end
 
-  # GenServer callbacks {{{
   @impl true
   def handle_call({:lookup, name}, _from, fileset) do
     {:reply, Kasten.FileSet.fetch(fileset, name), fileset}
   end
 
   @impl true
-  def handle_call({:scan_dir}, _from, _fileset) do
-    newset = Kasten.Scanner.scan_dir() |> Enum.reduce(%{}, &Kasten.FileSet.put(&2, &1))
+  def handle_call({:scan_dir, directory}, _from, _fileset) do
+    newset = Kasten.Scanner.scan_dir(directory) |> Enum.reduce(%{}, &Kasten.FileSet.put(&2, &1))
     {:reply, :ok, newset}
   end
   
